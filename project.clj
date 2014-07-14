7a3c3663fca0f92edb61d54343c212e5fb25bdec
Rick Hall <rickhall2000@hotmail.com>
2014-07-14 14:40:43 -0400

    updated dependencies

--- project.clj
+++ project.clj
@@ -2,12 +2,13 @@
   :description "A websocket client for a simulated stock ticker"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2268"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [om "0.6.5"]
+                 [jarohen/chord "0.4.2"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :source-paths ["src"]
 
