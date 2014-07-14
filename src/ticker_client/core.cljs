3cde5dda2024f9f1334fc19cf827cc4785c1d059
Rick Hall <rickhall2000@hotmail.com>
2014-07-14 16:48:05 -0400

    use om to display ticks

--- src/ticker_client/core.cljs
+++ src/ticker_client/core.cljs
@@ -1,18 +1,45 @@
 (ns ticker-client.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [chord.client :refer [ws-ch]]
+            [cljs.core.async :refer [<! >! put! close! chan]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (enable-console-print!)
+(def +ws-url+ "ws://localhost:8080/ticker")
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:text "Hello again!"
+                      :trades []}))
 
-(def ws (js/WebSocket. "ws://localhost:8080/ticker"))
-(aset ws "onmessage" (fn [m] (.log js/console (.-data m))) )
+(defn make-ticker [c]
+(go
+ (let [{:keys [ws-channel]} (<! (ws-ch +ws-url+))]
+   (loop []
+     (let [{:keys [message]} (<! ws-channel)]
+       (>! c message)
+       (recur))))))
 
-
+(defn master-view [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      (let [ticker (chan)]
+        (make-ticker ticker)
+        {:ticker ticker}))
+    om/IWillMount
+    (will-mount [_]
+      (let [ticker (om/get-state owner :ticker)]
+        (go (loop []
+              (let [tick (<! ticker)]
+                (om/transact! app :trades #(conj % tick)))
+              (recur)))))
+    om/IRenderState
+    (render-state [this state]
+      (dom/div nil
+       (dom/h1 nil (:text app))
+       (dom/h2 nil (pr-str (last (:trades app))))))))
 
 (om/root
-  (fn [app owner]
-    (dom/h1 nil (:text app)))
+ master-view
   app-state
   {:target (. js/document (getElementById "app"))})
